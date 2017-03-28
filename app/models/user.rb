86cc943310896bd816f6cb9292bf9d913cfa8826
jja <gitter@tutanota.com>
2017-03-27 19:37:41 -0600

    arregla mail de activación en desarrollo y agrega funcionalidad para subir imágenes

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,9 +78,9 @@ class User < ApplicationRecord
     def downcase_email
       email.downcase!
     end
-    
+
     def create_activation_digest
-      self.activation_token = User.new_token
+      self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
 end
